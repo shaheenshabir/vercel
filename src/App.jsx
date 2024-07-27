4aef35b524315272a0ebff51effb3f43d606746a
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-07-27 20:26:59 +0500

    vercel

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>HELLO WORLD</h1>
+     <img src="https://i0.wp.com/picjumbo.com/wp-content/uploads/magical-spring-forest-scenery-during-morning-breeze-free-photo.jpg?w=600&quality=80" />
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam expedita voluptatem voluptates quas labore voluptatum?</p>
+    </>
+  )
+}
+
+export default App
